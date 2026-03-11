fe48c5d79694d159c0fa3754790f15b3104cd79c
RFearon123 <rebeccafearon12@outlook.com>
2026-03-11 12:25:17 +0000

    trying to figure out my life submit function

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function submitItemsToChecklist () {
-    
+
+   let text = document.getElementsByClassName
+
 }
+
+/*
+ document.getElementById("myForm").addEventListener("submit", function(e) {
+  e.preventDefault(); // stop page reload
+
+  let text = document.getElementById("userText").value;
+
+  document.querySelector(".output").textContent = text;
+});
+ */
