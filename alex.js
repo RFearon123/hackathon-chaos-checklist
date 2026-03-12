68115df5f0d0d3c4a8d227f3c78fb9cba0b86f08
alexpalmer1308-spec <alex.palmer1308@gmail.com>
2026-03-12 10:41:58 +0000

    update js

--- alex.js
+++ alex.js
@@ -8,22 +8,44 @@ const clickChecker = document.body;
 let clickCounter = 0;
 let randomNum =  Math.floor(Math.random() * 10) + 1;
 console.log("random number " + randomNum)
-function toggleColour(){
-    let ele = document.body;
-   ele.classList.toggle("funkyColour")
+
+
+
+
+function randomNumber() {
+  return Math.floor(Math.random() * 255 + 1);
 }
+
 
 function clickCount(){
  clickCounter +=1;
  
 if(clickCounter === randomNum){
+  let rgb1 = randomNumber();
+  let rgb2 = randomNumber();
+  let rgb3 = randomNumber();
+  
+  clickChecker.style.backgroundColor = `rgb(${rgb1}, ${rgb2}, ${rgb3})`;
+
     randomNum = Math.floor(Math.random() * 10) + 1;
     console.log("random number " + randomNum)
-    toggleColour();
+    // toggleColour();
 
     clickCounter = 0;
 }
 }
+
+
+
+
+// function changeColour() {
+ 
+// }
+// function toggleColour(){
+//     let ele = document.body;
+//    ele.classList.toggle("funkyColour")
+// }
+
 
 
 
